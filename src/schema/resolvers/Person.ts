93f6c80de5596ab79fa411db00c44cdafc228045
Cory McAboy <cory.mcaboy@gmail.com>
2019-02-10 13:18:28 -0500

    added numRating, averagerating, and profilePic resolvers

--- src/schema/resolvers/Person.ts
+++ src/schema/resolvers/Person.ts
@@ -7,8 +7,11 @@ import { PersonResolvers } from "../../types/generated";
 export const Person: ResolverMap = {
   ...PersonResolvers.defaultResolvers,
   photos: ({ id: personId }, _, { dataSources }) => {
-    console.log("User.photos userId: ", personId);
     return dataSources.typeORM.findPhotos(personId);
+  },
+  profilePic: async ({ id: personId }, _, { dataSources }) => {
+    const photos = await dataSources.typeORM.findPhotos(personId);
+    return photos[0];
   },
   platform: ({ platformId }, _, { dataSources }) =>
     dataSources.typeORM.findPlatform(platformId),
@@ -21,5 +24,19 @@ export const Person: ResolverMap = {
     return reviews;
   },
   comments: ({ id: authorId }, _, { dataSources }) =>
-    dataSources.typeORM.findCommentsByAuthor(authorId)
+    dataSources.typeORM.findCommentsByAuthor(authorId),
+  numRatings: async ({ id: personId }, _, { dataSources }) => {
+    const reviews = await dataSources.typeORM.findReviews(personId);
+    return reviews.length;
+  },
+  averageRating: async ({ id: personId }, _, { dataSources }) => {
+    const reviews = await dataSources.typeORM.findReviews(personId);
+    if (!reviews.length) {
+      return 0;
+    }
+    return (
+      reviews.map(review => review.rating).reduce((prev, curr) => prev + curr) /
+      reviews.length
+    );
+  }
 };
